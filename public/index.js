2965c380398267420ddef5a5f881f8390f69fa9a
Roland Quast <rquast@rolandquast.com>
2021-10-04 12:03:32 +1000

    fix: more WIP fixes

--- public/index.js
+++ public/index.js
@@ -3,11 +3,8 @@ const helmet = require('helmet');
 const open = require('open');
 const corsProxy = require('@isomorphic-git/cors-proxy/middleware.js');
 
-function getContentSecurityPolicy(proxies) {
-  return `default-src 'self' ${proxies}; style-src 'self' 'unsafe-inline'; font-src 'self'; img-src 'self' data: blob: 'unsafe-eval';`;
-}
-
 const load = (
+  html = '',
   title = 'GitFeatures',
   description = 'GitFeatures: BDD specification writing tool',
   port = 25921,
@@ -32,35 +29,6 @@ const load = (
   },
   isLaunchingBrowser = true
 ) => {
-
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="utf-8" />
-      <meta http-equiv="Content-Security-Policy" content="${getContentSecurityPolicy(proxies)}">
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="/_dist_/index.css" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content="${description}" />
-      <meta name="gf:config" content="${encodeURIComponent(JSON.stringify(config))}" />
-      <title>${title}</title>
-      <style type="text/css">
-          html, body {
-              overscroll-behavior: contain;
-              overscroll-behavior-y: contain;
-              height: 100%;
-              margin: 0;
-          }
-      </style>
-    </head>
-    <body>
-    <div id="root"></div>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <script type="module" src="/_dist_/index.js"></script>
-    </body>
-    </html>
-  `;
 
   const app = express();
   app.use(helmet.dnsPrefetchControl());
